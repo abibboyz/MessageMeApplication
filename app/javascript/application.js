611c2242e36917aa0045e5cc9cc6e9af87f019e1
Abinash <abi.kh03@gmail.com>
2023-01-02 23:52:49 +1100

    minor custom relative module error removed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-$('.message .close')
-  .on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-    ;
-  });
+import jQuery from "jquery"
